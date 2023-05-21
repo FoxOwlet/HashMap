e5483e70627d5feb294162ef186d1f2f81a1d71f
foxowlet <foxowlet.it@gmail.com>
2023-05-21 12:38:51 +0200

    Part 2. Improvements: use dynamic array and binary search tree for buckets

--- src/main/java/com/foxowlet/demo/hashmap/HashMap.java
+++ src/main/java/com/foxowlet/demo/hashmap/HashMap.java
@@ -2,27 +2,109 @@ package com.foxowlet.demo.hashmap;
 
 public class HashMap<K, V> implements Map<K, V> {
     private Node[] values;
+    private int size;
+    private double threshold;
 
     public HashMap() {
         this.values = new Node[16];
+        this.size = 0;
+        this.threshold = 0.75;
     }
 
     @Override
     public void put(K key, V value) {
-        Node<K, V> node = new Node<>();
-        node.key = key;
-        node.value = value;
-        node.next = values[computeIndex(key)];
-        values[computeIndex(key)] = node;
+        int hash = key.hashCode();
+        int index = computeIndex(key);
+        Node<K, V> node = values[index];
+        Node<K, V> lastNode = null;
+        while (node != null) {
+            lastNode = node;
+            if (hash == node.hash) {
+                if (node.key.equals(key)) {
+                    node.value = value;
+                    return;
+                }
+                node = node.right;
+            } else if (hash < node.hash) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        Node<K, V> newNode = new Node<>();
+        newNode.hash = hash;
+        newNode.key = key;
+        newNode.value = value;
+        if (lastNode == null) {
+            values[index] = newNode;
+        } else if (hash < lastNode.hash) {
+            lastNode.left = newNode;
+        } else {
+            lastNode.right = newNode;
+        }
+        if (++size > values.length * threshold) {
+            resize();
+        }
+    }
+
+    private void resize() {
+        Node<K, V>[] oldNodes = values;
+        values = new Node[values.length * 2];
+        for (Node<K, V> oldNode : oldNodes) {
+            if (oldNode != null) {
+                addNode(oldNode);
+            }
+        }
+    }
+
+    private void addNode(Node<K, V> oldNode) {
+        int index = computeIndex(oldNode.key);
+        Node<K, V> node = values[index];
+        Node<K, V> lastNode = null;
+        while (node != null) {
+            lastNode = node;
+            if (oldNode.hash < node.hash) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        Node<K, V> newNode = new Node<>();
+        newNode.hash = oldNode.hash;
+        newNode.key = oldNode.key;
+        newNode.value = oldNode.value;
+        if (lastNode == null) {
+            values[index] = newNode;
+        } else if (oldNode.hash < lastNode.hash) {
+            lastNode.left = newNode;
+        } else {
+            lastNode.right = newNode;
+        }
+        if (oldNode.left != null) {
+            addNode(oldNode.left);
+        }
+        if (oldNode.right != null) {
+            addNode(oldNode.right);
+        }
     }
 
     @Override
     public V get(K key) {
+        int hash = key.hashCode();
         Node<K, V> node = values[computeIndex(key)];
-        while (node != null && !node.key.equals(key)) {
-            node = node.next;
+        while (node != null) {
+            if (hash == node.hash) {
+                if (key.equals(node.key)) {
+                    return node.value;
+                }
+                node = node.right;
+            } else if (hash < node.hash) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
         }
-        return node == null ? null : node.value;
+        return null;
     }
 
     private int computeIndex(K key) {
@@ -30,8 +112,10 @@ public class HashMap<K, V> implements Map<K, V> {
     }
 
     private static class Node<K, V> {
+        private int hash;
         private K key;
         private V value;
-        private Node<K, V> next;
+        private Node<K, V> left;
+        private Node<K, V> right;
     }
 }
--- src/main/java/com/foxowlet/demo/hashmap/Main.java
+++ src/main/java/com/foxowlet/demo/hashmap/Main.java
@@ -3,6 +3,45 @@ package com.foxowlet.demo.hashmap;
 
 public class Main {
     public static void main(String[] args) {
+        Map<BadKey, Integer> map = new HashMap<>();
+        for (int i = 0; i < 1_000_000; i++) {
+            map.put(new BadKey(i), i);
+        }
+        measureTime(() -> map.get(new BadKey(0)));
+        measureTime(() -> map.get(new BadKey(99_999)));
+        measureTime(() -> map.get(new BadKey(100_000)));
+    }
 
+    private static class BadKey {
+        private int value;
+
+        private BadKey(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            BadKey badKey = (BadKey) o;
+            return value == badKey.value;
+        }
+
+        @Override
+        public int hashCode() {
+            return value % 1000;
+        }
+    }
+
+    private static void measureTime(Runnable action) {
+        long runTime = 0;
+        int iterations = 10;
+        for (int i = 0; i < iterations; i++) {
+            long startTime = System.nanoTime();
+            action.run();
+            long endTime = System.nanoTime();
+            runTime += endTime - startTime;
+        }
+        System.out.printf("Elapsed time: %,d%n", runTime / iterations);
     }
 }
